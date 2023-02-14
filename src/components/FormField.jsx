ef6c36b7e42dd9dd9da1e1a325b2b2c4f6d35bf4
zedaenious <zed.dev.22@gmail.com>
2023-02-14 17:28:28 -0500

    fine tune home search

--- src/components/FormField.jsx
+++ src/components/FormField.jsx
@@ -3,11 +3,7 @@ import React from 'react'
 const FormField = ({labelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe }) => {
   return (
     <div>
-      <div className="flex items-center gap-2 mb-2">
-        <label
-          htmlFor={name}
-          className="block text-sm font-medium text-gray-900"
-        >{labelName}</label>
+      <div className="flex items-center gap-2">
         {isSurpriseMe && (
           <button
             type="button"
--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -82,7 +82,7 @@ const CreatePost = () => {
         <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
         <p className="mt-2 text-[#666e75] text-[16px] max-w[500px]">Create imaginative and visually stunning images through DALL-E AI and share them with the community</p>
       </div>
-      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+      <form className="mt-8 max-w-3xl" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-5">
           <FormField
             labelName="your name"
